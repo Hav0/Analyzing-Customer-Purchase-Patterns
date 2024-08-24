f11f50c786b00ddbaaeddda24e154c3b5d870b96
abdu <ana105@sfu.ca>
2024-08-23 22:23:52 -0700

    linked app file with external CSS

--- app.py
+++ app.py
@@ -4,23 +4,40 @@ import plotly.express as px
 
 # bar chart for product categories and purchase frequency
 
-bar_plot = px.bar(customer_data, x = 'preferred_category', y = 'purchase_frequency', color = 'gender',
+bar_plot = px.bar(customer_data, x = 'preferred_category', y = 'number of purchases (1 year)', color = 'gender',
                               title = "Purchase Frequencies based on Product Category and Gender")
 
 # box plot for income distribution and gender
 
-box_plot = px.box(customer_data, x = 'gender', y = 'income', 
+box_plot = px.box(customer_data, x = 'gender', y = 'income(thousands of dollars)', 
                   title = 'Distribution of Income Based on Gender')
 
-app = Dash(__name__)
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Poppins:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    }
+]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'Customer Segmentation Analytics: How Can We Learn More About Customer Purchase Data?'
 
 # Create the app layout
 app.layout = html.Div(
     children=[
-        html.H1(children="Customer Segmentation Analytics"),
-        html.P(children="Analyze customer purchase data based on age, gender, and income as well as which categories of products customers of various demographics prefer."),
-        dcc.Graph(figure = bar_plot), 
-        dcc.Graph(figure = box_plot),
+        html.Div(
+            children = [
+            html.H1(children="Customer Analytics Based on Demographic Segmentation", className= 'header-title'),
+            html.P(children="Analyzing customer purchase data based on their age, gender, and income as well as which categories of products customers of various demographics prefer."
+            ,className = 'description'), 
+            ],
+            className = 'header'
+        ),
+        dcc.Graph(figure = bar_plot, config={"displayModeBar": False}, className = 'bar-plot'),
+        dcc.Graph(figure = box_plot, config={"displayModeBar": False}, className = 'box-plot'),
     ]
 )
 
