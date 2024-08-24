967a6c9f8295b82a3f40f4e6db3885047eef65a7
abdu <ana105@sfu.ca>
2024-08-23 22:23:15 -0700

    renaming columns

--- cleaning.py
+++ cleaning.py
@@ -5,5 +5,6 @@ customer_data_null = pd.isnull(customer_data)
 customer_data.dropna()
 
 customer_data = customer_data.drop(['spending_score'], axis = 1)
+customer_data = customer_data.rename(columns = {'income':'income(thousands of dollars)', 'purchase_frequency': 'number of purchases (1 year)'})
 #print(customer_data)
 
